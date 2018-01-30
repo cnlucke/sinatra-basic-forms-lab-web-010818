485746b717d7c3e2c32ec90c26241c1cbef4f0cd
cnlucke <cristy.lucke@gmail.com>
2018-01-30 10:08:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    status 200
+    erb :create_puppy
+  end
+
+  post '/' do
+    erb :display_puppy
+  end
 end
